3676cc322679c9972b89d768235fd95d36575db7
Manivannan S <sharvamanivannan@gmail.com>
2023-08-14 13:37:44 +0000

    Changes in the about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,14 +5,14 @@ import aboutImg from '../Images/aboutImg.png'
 const About = () => {
     return (
         <section className="about" id="about">
-            <div className="container col-xxl-8 px-4 py-5">
+            <div className="container col-xxl-8 py-5">
                 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                     <div className="col-10 col-sm-8 col-lg-6">
                         <img src={aboutImg} style={{ borderRadius: '1.7rem' }} className="d-block mx-lg-auto img-fluid" alt="mee" width="700" height="500" loading="lazy" />
                     </div>
                     <div className="col-lg-6">
                         <h3 style={{ color: "#147efb", fontSize: '1.7rem', textTransform: 'uppercase' }}>About me</h3>
-                        <h4 className="fw-bold lh-1 mb-3" style={{ textTransform: 'uppercase', textAlign: 'justify' }}>A dedicated Full Stack Developer <br /> based in Bangalore, Karnataka 📍</h4>
+                        <h4 className="fw-bold lh-1 mb-3" style={{ textAlign: 'justify' }}>A dedicated Full Stack Developer <br /> based in Bangalore, Karnataka 📍</h4>
                         <p className="lead" style={{ textAlign: 'justify' }}>As a Full Stack Developer, I possess an impressive arsenal of skills in HTML,
                             CSS, JavaScript, React, Tailwind, and SCSS. I excel in designing and maintaining
                             responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic,
