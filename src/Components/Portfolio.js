aac7c1cbb181b6a30bcf6fd20d1b3f47b253ccfb
Manivannan S <sharvamanivannan@gmail.com>
2023-08-17 13:58:54 +0000

    changes in portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import pizzeria from '../Images/pizzeria.png'
 
 const Portfolio = () => {
   return (
@@ -6,14 +7,24 @@ const Portfolio = () => {
       <div className='container py-3'>
         <h3 style={{ color: "#147efb", fontSize: '1.8rem', textTransform: 'uppercase' }}>Porfolio</h3>
         <h4 className="fw-bold lh-1 mb-3" style={{ paddingTop: '1.5rem', textAlign: 'left' }}>Each project is a unique piece of development 🧩</h4>
-        {/* nebula.cloud */}
-        {/* wear-it */}
-        {/* vegman */}
-        {/* chisle-gym */}
-        {/* stream */}
-        {/* n-cosmetics */}
+        What Happened
+        <div class="container col-xxl-8 px-4 py-5">
+          <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+              <img src={pizzeria} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+            </div>
+            <div class="col-lg-6">
+              <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+              <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </section >
   )
 }
 
